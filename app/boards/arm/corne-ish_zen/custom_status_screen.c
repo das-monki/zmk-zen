c7a1e9d600e4387f46f85bc6a4aff514d9ff9f14
Biggie <>
2021-12-13 20:22:47 +0100

    Comment out layer-status

--- app/boards/arm/corne-ish_zen/custom_status_screen.c
+++ app/boards/arm/corne-ish_zen/custom_status_screen.c
@@ -46,34 +46,34 @@ lv_obj_t *zmk_display_status_screen() {
                  41);
 #endif
 
-#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
-    //lv_style_set_pad_inner(&layerstyle, LV_STATE_DEFAULT, 12);
-    //lv_obj_add_style(&layer_status_widget, LV_WIDGET_PART_MAIN, &layerstyle);
-    zmk_widget_layer_status_init(&layer_status_widget, screen);
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), NULL, LV_ALIGN_IN_BOTTOM_MID, 0,
-                 -5);
-#endif
+/* #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS) */
+/*     //lv_style_set_pad_inner(&layerstyle, LV_STATE_DEFAULT, 12); */
+/*     //lv_obj_add_style(&layer_status_widget, LV_WIDGET_PART_MAIN, &layerstyle); */
+/*     zmk_widget_layer_status_init(&layer_status_widget, screen); */
+/*     lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), NULL, LV_ALIGN_IN_BOTTOM_MID, 0, */
+/*                  -5); */
+/* #endif */
 
-#if CONFIG_BOARD_CORNEISH_ZEN_RIGHT
+/* #if CONFIG_BOARD_CORNEISH_ZEN_RIGHT */
     lv_obj_t * zenlogo_icon;
     zenlogo_icon = lv_img_create(screen, NULL);
     lv_img_set_src(zenlogo_icon, &zenlogo);
     lv_obj_align(zenlogo_icon, NULL, LV_ALIGN_IN_BOTTOM_MID, 2, -5);
-#endif
+/* #endif */
 
-#if CONFIG_BOARD_CORNEISH_ZEN_LEFT
-#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
-    lv_obj_t * LayersHeading;
-    LayersHeading = lv_img_create(screen, NULL);
-    lv_obj_align(LayersHeading, NULL, LV_ALIGN_IN_BOTTOM_MID, 8, 5);
-    lv_img_set_src(LayersHeading, &layers2);
-#else
-    lv_obj_t * zenlogo_icon;
-    zenlogo_icon = lv_img_create(screen, NULL);
-    lv_img_set_src(zenlogo_icon, &zenlogo);
-    lv_obj_align(zenlogo_icon, NULL, LV_ALIGN_IN_BOTTOM_MID, 2, -5);
-#endif
-#endif
+/* #if CONFIG_BOARD_CORNEISH_ZEN_LEFT */
+/* #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS) */
+/*     lv_obj_t * LayersHeading; */
+/*     LayersHeading = lv_img_create(screen, NULL); */
+/*     lv_obj_align(LayersHeading, NULL, LV_ALIGN_IN_BOTTOM_MID, 8, 5); */
+/*     lv_img_set_src(LayersHeading, &layers2); */
+/* #else */
+/*     lv_obj_t * zenlogo_icon; */
+/*     zenlogo_icon = lv_img_create(screen, NULL); */
+/*     lv_img_set_src(zenlogo_icon, &zenlogo); */
+/*     lv_obj_align(zenlogo_icon, NULL, LV_ALIGN_IN_BOTTOM_MID, 2, -5); */
+/* #endif */
+/* #endif */
 
     //lv_task_handler();
     lv_refr_now(NULL);
